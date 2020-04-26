d74f8ceab0ec7fda616493c24531615eb7da7ac9
Nathan E. Arnold <n@wrynst.com>
2020-04-26 10:25:25 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <app-navigation></app-navigation>
+
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+            <hello-world></hello-world>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+import AppNavigation from '@/components/AppNavigation';
+
+export default {
+    name: 'App',
+    components: {
+        AppNavigation
+        
+    }
+};
+</script>
+
+<style>
+</style>
